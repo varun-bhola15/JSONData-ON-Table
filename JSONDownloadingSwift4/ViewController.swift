324f4d18737cd028aba12257493b724191d2ee01
varun-bhola15 <varundude.bhola@gmail.com>
2018-01-08 13:16:24 +0530

    Initial Commit

---
+++ JSONDownloadingSwift4/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JSONDownloadingSwift4
+//
+//  Created by varun bhola on 08/01/18.
+//  Copyright © 2018 varun15bhola. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
